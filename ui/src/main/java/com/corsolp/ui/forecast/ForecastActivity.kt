79fb3ebdba40df5f65f10a7ebda71c3d705f5a45
giuliavanni <giulia.vanni6@studio.unibo.it>
2025-05-14 23:34:07 +0200

    created forecast activity

---
+++ ui/src/main/java/com/corsolp/ui/forecast/ForecastActivity.kt
@@ -0,0 +1,51 @@
+package com.corsolp.ui.forecast
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.corsolp.ui.R
+
+
+class ForecastActivity : AppCompatActivity() {
+
+    private lateinit var forecastRecyclerView: RecyclerView
+    private lateinit var forecastAdapter: ForecastAdapter
+    private lateinit var viewModel: ForecastViewModel
+
+    private lateinit var city: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forecast)
+
+        forecastRecyclerView = findViewById(R.id.forecastRecyclerView)
+        forecastAdapter = ForecastAdapter(emptyList())
+        forecastRecyclerView.layoutManager = LinearLayoutManager(this)
+        forecastRecyclerView.adapter = forecastAdapter
+
+        city = intent.getStringExtra("city_name") ?: ""
+
+        if (city.isEmpty()) {
+            Toast.makeText(this, "Nessuna città ricevuta.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        viewModel = ViewModelProvider(this)[ForecastViewModel::class.java]
+
+        viewModel.forecastList.observe(this) { forecastItems ->
+            forecastAdapter.updateData(forecastItems)
+        }
+
+        viewModel.error.observe(this) { errorMessage ->
+            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+        }
+
+        viewModel.loadForecast(city)
+    }
+}
+
+
+
