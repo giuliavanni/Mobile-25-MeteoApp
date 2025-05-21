c3944598a56c410bd897d5f01ee77d24d97fae11
giuliavanni <giulia.vanni6@studio.unibo.it>
2025-05-21 16:01:37 +0200

    created a data class to store weather data in an object

---
+++ data/src/main/java/com/corsolp/data/WeatherInfo.kt
@@ -0,0 +1,10 @@
+package com.corsolp.data
+
+data class WeatherInfo(
+    val cityName: String,
+    val temperature: Double,
+    val description: String,
+    val iconUrl: String,
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0
+)
