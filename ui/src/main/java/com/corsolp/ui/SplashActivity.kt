660b57454660cd9e7cad4d60d59aba6a1755001d
giuliavanni <giulia.vanni6@studio.unibo.it>
2025-05-21 15:31:37 +0200

    added animation the the splash activity

--- ui/src/main/java/com/corsolp/ui/SplashActivity.kt
+++ ui/src/main/java/com/corsolp/ui/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
 import com.corsolp.ui.databinding.ActivitySplashBinding
 import com.corsolp.ui.mainactivity.MainActivity
 
@@ -20,12 +21,20 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val lottieAnimationView = findViewById<LottieAnimationView>(R.id.lottieAnimationView)
+
+        // Avvia l'animazione
+        lottieAnimationView.playAnimation()
+
+        // Configura se l'animazione deve iniziare automaticamente o meno
+        lottieAnimationView.loop(true)
+
 
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             //Se voglio provare l'activity con la demo di richiesta permessi
             //val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
-        }, 500)
+        }, 1500)
     }
 }
