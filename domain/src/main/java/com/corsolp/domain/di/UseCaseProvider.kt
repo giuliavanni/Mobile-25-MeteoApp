6886faa691009bbac70cbf03b1d7272c07d7a406
giuliavanni <giulia.vanni6@studio.unibo.it>
2025-05-25 14:50:46 +0200

    Refactor: Set up UseCaseProvider using concrete UseCase classes

--- domain/src/main/java/com/corsolp/domain/di/UseCaseProvider.kt
+++ domain/src/main/java/com/corsolp/domain/di/UseCaseProvider.kt
@@ -1,24 +1,30 @@
 package com.corsolp.domain.di
 
-import com.corsolp.domain.usecase.FetchAccomodationTypeListUseCase
-import com.corsolp.domain.usecase.FetchAccomodationTypeListUseCaseImpl
-import com.corsolp.domain.usecase.StartFUseCase
-import com.corsolp.domain.usecase.StartFUseCaseImpl
+import com.corsolp.domain.usecase.DeleteCityUseCase
+import com.corsolp.domain.usecase.FetchForecastUseCase
+import com.corsolp.domain.usecase.FetchWeatherByCoordinatesUseCase
+import com.corsolp.domain.usecase.FetchWeatherUseCase
+import com.corsolp.domain.usecase.GetSavedCitiesUseCase
+import com.corsolp.domain.usecase.SaveCityUseCase
 
 object UseCaseProvider {
 
-    lateinit var fetchAccomodationTypeListUseCase: FetchAccomodationTypeListUseCase
-    lateinit var startFUseCase: StartFUseCase
+    lateinit var fetchWeatherUseCase: FetchWeatherUseCase
+    lateinit var fetchForecastUseCase: FetchForecastUseCase
+    lateinit var fetchWeatherByCoordinatesUseCase: FetchWeatherByCoordinatesUseCase
+    lateinit var saveCityUseCase: SaveCityUseCase
+    lateinit var getSavedCitiesUseCase: GetSavedCitiesUseCase
+    lateinit var deleteCityUseCase: DeleteCityUseCase
 
     fun setup(
         repositoryProvider: RepositoryProvider
     ) {
-        fetchAccomodationTypeListUseCase = FetchAccomodationTypeListUseCaseImpl(
-            accomodationRepository = repositoryProvider.accomodationRepository
-        )
+        fetchWeatherUseCase = FetchWeatherUseCase(repositoryProvider.weatherRepository)
+        fetchForecastUseCase = FetchForecastUseCase(repositoryProvider.forecastRepository)
+        fetchWeatherByCoordinatesUseCase = FetchWeatherByCoordinatesUseCase(repositoryProvider.weatherRepository)
 
-        startFUseCase = StartFUseCaseImpl(
-            accomodationRepository = repositoryProvider.accomodationRepository
-        )
+        saveCityUseCase = SaveCityUseCase(repositoryProvider.cityRepository)
+        getSavedCitiesUseCase = GetSavedCitiesUseCase(repositoryProvider.cityRepository)
+        deleteCityUseCase = DeleteCityUseCase(repositoryProvider.cityRepository)
     }
 }
