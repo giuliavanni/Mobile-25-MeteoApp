db85b0b77d517403c1231fa662ac733d0196bdc2
giuliavanni <giulia.vanni6@studio.unibo.it>
2025-06-05 17:29:21 +0200

    Added coroutines to SplashActivity

--- ui/src/main/java/com/corsolp/ui/SplashActivity.kt
+++ ui/src/main/java/com/corsolp/ui/SplashActivity.kt
@@ -5,35 +5,32 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.airbnb.lottie.LottieAnimationView
 import com.corsolp.ui.databinding.ActivitySplashBinding
 import com.corsolp.ui.mainactivity.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
-
-    private val TAG = "MainActivity_TAG"
+    private val TAG = "SplashActivity_TAG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate")
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val lottieAnimationView = findViewById<LottieAnimationView>(R.id.lottieAnimationView)
-
-        // Avvia l'animazione
         lottieAnimationView.playAnimation()
-
-        // Configura se l'animazione deve iniziare automaticamente o meno
         lottieAnimationView.loop(true)
 
-
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        // Usando coroutine per il delay non bloccante
+        lifecycleScope.launch {
+            delay(2000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
-        }, 1500)
+        }
     }
 }
